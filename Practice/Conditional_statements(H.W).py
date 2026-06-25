4d815f070f697116f8d673807578021797a323e8
rohissumaiya84 <sumaiyarohis384@gmail.com>
2026-06-25 15:53:15 +0600

    add

---
+++ Practice/Conditional_statements(H.W).py
@@ -0,0 +1,36 @@
+'''
+taka = 15.2
+if taka > 40:
+    print('Learn python')
+else:
+    print("ML")
+'''
+
+marks =35
+
+if marks>40:
+    print("Your pass.")
+else:
+    print("Your gadha")
+
+
+
+subject= input("Enter your subject name:  ")
+
+if subject== 'java':
+    print('Learn OOP')
+elif subject == 'python':
+    print('Learn Mechine Learning')
+elif subject == 'mySQL':
+    print('Learn Database')
+else:
+    print('Learn C#')
+
+
+a=int(input("Enter 1st number: "))
+b= int(input("Enter 2nd number: "))
+
+if a>b:
+    print("I'm ready to go Canada ")
+else:
+    print("Stay in Bangladesh")
